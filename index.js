69ac55a35525875ac9a782c4b564e9a3389718c5
Tomi Savolainen <tomi.savolainen@orangit.fi>
2019-04-11 10:12:29 +0300

    add home route

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World!!!'));
+app.get('/home', (req, res) => res.send('Hello Home!!!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
